a0a9f3cb1daa8feac83b54f946191fd961b791e4
DAODEJI <92527006+DAODEJI@users.noreply.github.com>
2022-10-24 07:54:28 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from douban import url_generator
 from douban import main
 import time
 
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (HTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
 }
 url = url_generator()
 for i in url:
