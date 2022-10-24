73ab3c4063e934a5e23c384030e4284c2cf552fd
DAODEJI <daodeji@gmail.com>
2022-10-24 18:19:56 +0800

    修改headers中拼写错误（KHTML-HTML）

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import requests
 from lxml import etree
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (HTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
     }
 re = requests.get('https://movie.douban.com/top250', headers=headers).text
 
