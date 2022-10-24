869e9a138850f17282c9b226d9800d7dc6cfc6e2
DAODEJI <daodeji@gmail.com>
2022-10-24 18:21:58 +0800

    修改格式

--- douban.py
+++ douban.py
@@ -22,17 +22,15 @@ def main(url, headers):
         symbol = ','
         move_name = 'normalize-space(//*[@id="content"]/div/div[1]/ol/li[{}]/div/div[2]/div[1]/a/span[1])'.format(i)
         style = 'normalize-space(//*[@id="content"]/div/div[1]/ol/li[{}]/div/div[2]/div[2]/p[1]/text()[2])'.format(i)
-        Director_starring = 'normalize-space(//*[@id="content"]/div/div[1]/ol/li[{}]/div/div[2]/div[2]/p[1]/text()[1])'.format(i)
+        Director_star = 'normalize-space(//*[@id="content"]/div/div[1]/ol/li[{}]/div/div[2]/div[2]/p[1]/text()[1])'.format(i)
         score = 'normalize-space(//*[@id="content"]/div/div[1]/ol/li[{}]/div/div[2]/div[2]/div/span[2]//text())'.format(i)
         result1 = xp.xpath(move_name)
         test.append(str(result1))
         result3 = xp.xpath(style)
         test.append(str(result3))
-        result4 = xp.xpath(Director_starring)
+        result4 = xp.xpath(Director_star)
         test.append(str(result4))
         result5 = xp.xpath(score)
         test.append(str(result5))
         f.write(symbol.join(test) + '\n')
     f.close()
-
-
