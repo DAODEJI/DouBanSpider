9c3e1cd5695a6be29f61cc8a32b376d0ebca4d69
DAODEJI <daodeji@gmail.com>
2022-10-24 18:15:28 +0800

    更新headers格式

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import requests
 from lxml import etree
 
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
-}
-re = requests.get('https://movie.douban.com/top250',headers=headers).text
-
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
+    }
+re = requests.get('https://movie.douban.com/top250', headers=headers).text
 
 xp = etree.HTML(re)
 test2 = []
@@ -14,7 +14,8 @@ for i in range(1, 26):
     symbol = ','
     move_name = 'normalize-space(//*[@id="content"]/div/div[1]/ol/li[{}]/div/div[2]/div[1]/a/span[1])'.format(i)
     style = 'normalize-space(//*[@id="content"]/div/div[1]/ol/li[{}]/div/div[2]/div[2]/p[1]/text()[2])'.format(i)
-    Director_starring = 'normalize-space(//*[@id="content"]/div/div[1]/ol/li[{}]/div/div[2]/div[2]/p[1]/text()[1])'.format(i)
+    Director_starring = 'normalize-space(//*[@id="content"]/div/div[1]/ol/li[{}]/div/div[2]/div[2]/p[1]/text()[1])'.format(
+        i)
     score = 'normalize-space(//*[@id="content"]/div/div[1]/ol/li[{}]/div/div[2]/div[2]/div/span[2]//text())'.format(i)
     result1 = xp.xpath(move_name)
     a = str(result1)
