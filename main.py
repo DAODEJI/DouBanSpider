4bd9f9ad0ef9aa1abe4f0592bcd04e01180296c7
DAODEJI <daodeji@gmail.com>
2022-10-25 21:44:02 +0800

    修改

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from douban import main
 import time
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (HTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
+    'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36 Edg/106.0.1370.52'
+
 }
 url = url_generator()
 for i in url:
